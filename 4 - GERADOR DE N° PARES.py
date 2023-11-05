4e78df17235dc794f402f515cd9099ffb55efcf0
Marina Cindra <109001097+maricindra@users.noreply.github.com>
2023-11-05 09:53:21 -0300

    GERADOR DE N° PARES.py

--- 4 - GERADOR DE N° PARES.py
+++ 4 - GERADOR DE N° PARES.py
@@ -1,12 +1,13 @@
 #GERADOR DE NUMEROS PARES
 
+#GERADOR DE NUMEROS PARES
+
 a = int(input("Gerador de números pares:\n Qual seu número de inicio?\n"))
 b= int(input("Ate quanto?"))
 
 if b%2 == 0:
   b +=1
-else:
-  b=b
+
 
 if a%2 != 0:
   a += 1 
